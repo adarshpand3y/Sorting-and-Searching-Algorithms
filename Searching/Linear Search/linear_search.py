94d365d7de68b471d13db5f17f6cf52684fc57f2
unknown <Adarsh Pandey>
2021-03-26 16:55:12 +0530

    Added linear search in python

---
+++ Searching/Linear Search/linear_search.py
@@ -0,0 +1,20 @@
+length = int(input("Enter the length of the list: "))
+arr = []
+found = False
+
+# Input
+for i in range(length):
+    arr.append(int(input("Enter a number: ")))
+
+target = int(input("Enter number to search: "))
+
+for iteration, num in enumerate(arr):
+    if num == target:
+        found = True
+        pos = iteration
+        break
+
+if found:
+    print(f"Found at position {pos}")
+else:
+    print("Not found")
